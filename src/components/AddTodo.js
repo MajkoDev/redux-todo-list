eb700a44d4b08eb28ea4a02c8290484c031a8db4
Marian <hellomariandev@gmail.com>
2022-07-05 22:15:19 +0200

    finished AddTodo.js

--- src/components/AddTodo.js
+++ src/components/AddTodo.js
@@ -4,17 +4,34 @@ import { useState } from "react";
 const AddTodo = () => {
   const [value, setValue] = useState("");
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setValue('')
+  };
+
   const handleInput = (e) => {
     setValue(e.target.value);
   };
 
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <Flex>
         <FormControl>
-          <Input type="text" onChange={handleInput} />
+          <Input
+            type="text"
+            value={value}
+            onChange={handleInput}
+            borderTopRightRadius={0}
+            borderBottomRightRadius={0}
+          />
         </FormControl>
-        <Button colorScheme="teal" type="submit" disabled={!value}>
+        <Button
+          colorScheme="teal"
+          type="submit"
+          disabled={!value}
+          borderTopLeftRadius={0}
+          borderBottomLeftRadius={0}
+        >
           Add Todo
         </Button>
       </Flex>
